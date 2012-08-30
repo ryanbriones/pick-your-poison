118684a8ae696c1729c84e0ab589c1d6ce559eb5
Juan-Pablo Velez <jpvelez@gmail.com>
2012-08-30 16:42:15 -0500

    tweaked init

--- config/initializers/00-setup_mongo.rb
+++ config/initializers/00-setup_mongo.rb
@@ -1,19 +1,14 @@
 require 'yaml'
 require 'mongo'
 
-begin
-    config = YAML.load_file('config/database.yml')
-    puts 'got config from local'
-rescue
-    config = {
-        'server' => ENV['MONGO_FULL'],
-        'port' => ENV['MONGO_PORT'],
-        'db' => ENV['MONGO_DB'],
-        'user' => ENV['MONGO_USER'],
-        'pass' => ENV['MONGO_PASS']
-    }
-    puts 'got config from heroku env'
-end
+config = {
+    'server' => ENV['MONGO_FULL'],
+    'port' => ENV['MONGO_PORT'],
+    'db' => ENV['MONGO_DB'],
+    'user' => ENV['MONGO_USER'],
+    'pass' => ENV['MONGO_PASS']
+}
+puts 'got config from heroku env'
 
 conn = Mongo::Connection.new(config['server'], config['port'])  # Open connection
 puts conn
