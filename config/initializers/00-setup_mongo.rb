4bcfeea8a8bea294c6e12bb0dbd3f8dca3e102fd
Juan-Pablo Velez <jpvelez@gmail.com>
2012-09-13 19:38:00 -0500

    tweaked intializer to authenticate with both dbs

--- config/initializers/00-setup_mongo.rb
+++ config/initializers/00-setup_mongo.rb
@@ -10,8 +10,10 @@ rescue
         'port' => ENV['MONGO_PORT'],
         'tweets_db' => ENV['MONGO_TWEETS_DB'],
         'forms_db' => ENV['MONGO_FORMS_DB'],
-        'user' => ENV['MONGO_USER'],
-        'pass' => ENV['MONGO_PASS']
+        'tweets_user' => ENV['MONGO_TWEETS_USER'],
+        'tweets_pass' => ENV['MONGO_TWEETS_PASS'],
+        'forms_user' => ENV['MONGO_FORMS_USER'],
+        'forms_pass' => ENV['MONGO_FORMS_PASS']
     }
 
     heroku_config = true
@@ -29,8 +31,9 @@ forms_db = conn.db(config['forms_db'])
 puts config['forms_db'], forms_db
 
 if heroku_config == true
-    auth = db.authenticate(config['user'], config['pass'])  # Authenticate if on production server
-    puts auth
+    tweet_auth = tweet_db.authenticate(config['tweets_user'], config['tweets_pass'])  # Authenticate if on production server
+    forms_auth = tweet_db.authenticate(config['forms_user'], config['forms_pass'])  
+    puts tweet_auth, forms_auth
 end
 
 TWEETS = tweets_db['fp_tweets_chi_01'] # Connect to collections
